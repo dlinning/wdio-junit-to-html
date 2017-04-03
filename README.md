874214190afcfa78ba85473cfcaa8b53a24fae98
Linning <Doug.Linning@jockey.com>
2017-04-03 15:41:21 -0500

    Updated to v3.0.0, major changes.

--- README.md
+++ README.md
@@ -1,18 +1,32 @@
-#wdio-junit-to-html
+# wdio-junit-to-html
 
 This is meant to be used with Webdriverio's JUnit reporter.
 
-##NOTE: Version 3.0.0 is the lowest working version.
+A simple CLI tool to convert generated JUnit test results to HTML reports.
 
-###Usage
+### NOTE: Version 3.0.0 is the lowest working version.
+
+---
+
+### Usage
 This should be added to your `package.json`, as follows:
 
-####Please install this globally, or the syntax below will not work.
+#### Please install this globally, or the syntax below will not work.
 
+Note: `-i` and `-o` are required, otherwise default paths will be used.
 ```
 "scripts": {
     ...
-    "test": "wdio && wdio-junit-to-html -i <INPUT_DIRECTORY> -o <OUTPUT_DIRECTORY> [-l <ENABLE_LOGGING>]",
+    "test": "wdio && wdio-junit-to-html -i <INPUT_DIRECTORY> -o <OUTPUT_DIRECTORY>",
     ...
 },
 ```
+
+## Options
+`-i, --input <input>` The input folder path (relative)
+
+`-o, --output <output_directory>` The output folder path (relative)
+
+`-s, --single` Export all reports as one file.
+
+`-l, --log` Log basic information about what is being done.
--- index.js
+++ index.js
@@ -2,32 +2,42 @@
 var program = require('commander');
 const fs = require('fs');
 const xml2js = require('xml2js');
+const dateFormat = require('dateformat');
 
-var outputDir = 'output';
+const HTMLVars = require('./vars.js');
+
+var d = new Date();
+const generationDate = dateFormat(d,'isoDate');
+
+var outputDir = 'reports/output';
 var inputDir = 'reports';
 //How far back the path should go for source files
-//3 seems to be the limit
 var srcFileDepth = 3;
 
+var menu = [];menu.push('TEMP');
+var menuID = 0;
+var failed = 0;
+var passed = 0;
 
 program
-.option('-i, --input <input>', 'The user to authenticate as')
-.option('-o, --output <output>', 'The user\'s password')
-.option('-l, --log <log>','If used, will let you know when done running.')
+.usage('[options]')
+.option('-i, --input <input>', 'The input folder path (relative)')
+.option('-o, --output <output>', 'The output folder path (relative))
+.option('-s, --single','Export all reports in one file')
+.option('-l, --log','Log basic information about what is being done.')
 .parse(process.argv);
 
-//Actual code that runs
+//Actual code that runs when module called
 if(program.input)
     inputDir = program.input;
 if(program.output)
     outputDir = program.output;
-generateFromFolder();
-if(program.log)
-    console.log('HTML Reports Generated');
 
+generateFromFolder(); //Handles `program.single` within
 
+//Functions Below
 function parse(data){
-    var output = '<html lang="en"><head><meta charset="UTF-8"><title>JUnit Test Results - junit-to-html</title><script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script><style>html{font-family:Arial,sans-serif,monospace}.suite,.suite-container{position:relative;display:flex;flex-direction:column;border:1px solid #000;padding:8px;margin:8px 16px}.suite .title{font-size:18px;display:block;padding:4px 2px;background:#4CAF50}.suite>.duration,[class^=count_]{font-size:14px}.has_error,.title.has_error{background:#F44336}.tests{border:1px solid #000;padding:8px;max-height:24px;overflow-y:hidden;transition:.3s ease max-height}.tests .expand_tests{display:block;padding:4px 2px;font-size:16px;font-weight:700;text-decoration:underline}.expand_tests::before{display:block;content:\'Expand Tests ▼\'}.tests.open{max-height:99999999px}.tests.open .expand_tests::before{content:\'Close Tests ▲\'}.tests .expand_tests:hover{background:#7EA6D7;cursor:pointer}.test{display:flex;flex-direction:column;border:1px solid #b6b6b6;margin:8px 0;padding:4px}.test .err_out{background:rgba(0,0,0,.2);padding:8px;margin:0 8px;border-radius:4px}</style></head><body>';
+    var output = '';
 
     for(s in data['testsuites']['testsuite']){
         var suite = data['testsuites']['testsuite'][s];
@@ -41,12 +51,14 @@ function parse(data){
             }
 
             output += `
-<div class="suite-container">
-    <span class="title"><b>`+JSON.stringify(suite['properties'][0]['property'][1]['$']['value'])+`</b></span>
+<div class="suite-container" id="suite_`+menuID+`">
+    <span class="title"><b>`+suite['properties'][0]['property'][1]['$']['value']+`</b></span>
     <span class="suite-duration"><b>Run Duration: </b>`+suite['$']['time']+`seconds</span>
     <span class="suite-fromfile"><b>Source File: </b>`+outFp+`</span>
     <span class="suite-spec"><b>Spec ID: </b>`+suite['properties'][0]['property'][0]['$']['value']+`</span>
 `;
+            menu.push('<li class="head"><a href="#suite_'+menuID+'">'+suite['properties'][0]['property'][1]['$']['value']+'</a></li>');
+            menuID++;
         } else {
             // All "sub-suites"
 
@@ -63,15 +75,17 @@ function parse(data){
 
             //Begin outputting test suite to html
             output += `
-<div class="suite">
-    <span class="title `+has_error+`"><b>Suite: </b>`+name+`</span>
-    <span class="duration"><b>Run Duration: </b>`+op_time+`</span>
-    <span class="count_tests"><b>Test Count: </b>`+count_tests+`</span>
-    <span class="count_fails"><b>Failed Test Count: </b>`+count_fails+`</span>
-    <span class="count_errors"><b>Error Count: </b>`+count_errors+`</span>
-    <span class="count_skips"><b>Skipped Test Count: </b>`+count_skips+`</span>
-    <div class="tests">
-        <span class="expand_tests"></span>`;
+    <div class="suite" id="sub_`+menuID+`">
+        <span class="title `+has_error+`"><b>Suite: </b>`+name+`</span>
+        <span class="duration"><b>Run Duration: </b>`+op_time+`</span>
+        <span class="count_tests"><b>Test Count: </b>`+count_tests+`</span>
+        <span class="count_fails"><b>Failed Test Count: </b>`+count_fails+`</span>
+        <span class="count_errors"><b>Error Count: </b>`+count_errors+`</span>
+        <span class="count_skips"><b>Skipped Test Count: </b>`+count_skips+`</span>
+        <div class="tests">
+            <span class="expand_tests"></span>`;
+            menu.push('<li class="sub"><a href="#sub_'+menuID+'">'+suite['properties'][0]['property'][1]['$']['value']+'</a></li>');
+            menuID++;
             //Output the individual tests
             for(var t in suite['testcase']){
                 var test = suite['testcase'][t];
@@ -81,31 +95,43 @@ function parse(data){
                 if(test['error'] != null){
                     error = '<i style=\"color:red;\">'+test['error'][0]['$']['message']+'</i>';
                     err_out = '<pre class="err_out">'+test['system-err']+'</pre>';
+                    failed++;
                 } else {
                     error = "<i style=\"color:green;\">PASS</i>";
+                    passed++;
                 }
 
                 output += `
-<div class="test">
-    <span class="name"><b>Name: </b>`+test['$']['name']+`</span>
-    <span class="duration"><b>Run Time: </b>`+test['$']['time']+`seconds</span>
-    <span class="result"><b>Result: </b>`+error+`</span>
-    <span>`+err_out+`</span>
-</div>`;
+    <div class="test" id="test_`+menuID+`">
+        <span class="name"><b>Name: </b>`+test['$']['name']+`</span>
+        <span class="duration"><b>Run Time: </b>`+test['$']['time']+`seconds</span>
+        <span class="result"><b>Result: </b>`+error+`</span>
+        <span>`+err_out+`</span>
+    </div>`;
+                var menuErr = (test['error'] != null) ? ' error' : '';
+                menu.push('<li class="test'+menuErr+'"><a href="#test_'+menuID+'">'+test['$']['name']+'</a></li>');
+                menuID++;
             }
 
             output += '</div></div>';
         }
 
     }
-    output += '</div><!--Closes `.suite-container`-->';
-
-    output += "</body><script>$(function(){$('.expand_tests').on('click',function(){$(this).parent().toggleClass('open');});});</script></html>";
+    output += '</div>\n<!--Closes `.suite-container`-->';
 
     return output;
 }
 
-function convertToHTML(filePath){
+function cleanMenu(){
+    //"Finishes" the report, adds `#content`
+    //MUST SET `menu = [];` AFTER CALLING THIS AND USING `menu` SOMEWHERE.
+    menu[0] = '<li id="nav_counts">Report: '+generationDate+'<br><span class="has_error">FAILED: '+failed+'</span><br><br><span>PASSED: '+passed+'</span><br></li>';
+    menu.push('<li style="padding:8px 16px;"><button onclick="openall()">Open/Close all Tests</button></li></ul></nav>\n<div id="content">');
+    failed = 0;
+    passed = 0;
+}
+
+function convertSingleFileToHTML(filePath){
     var output = filePath;
     var pathSplit = filePath.split('\\');
     fs.readFile(inputDir+'/'+filePath, 'utf8', function (err,data) {
@@ -113,9 +139,14 @@ function convertToHTML(filePath){
             return console.log(err);
         }
         xml2js.parseString(data,function(err,result){
+            var res = parse(result);
+
+            //Finish up the menu
+            cleanMenu();
+
             fs.writeFile(
                 outputDir+'/'+pathSplit[pathSplit.length-1].substr(0,pathSplit[pathSplit.length-1].length-4)+'.html',
-                parse(result),
+                HTMLVars.HTMLHeader+menu.join('')+res+HTMLVars.HTMLFooter, //Actually parse the input here
                 function(err){
                     if(err){
                         return console.log(err);
@@ -123,20 +154,69 @@ function convertToHTML(filePath){
                 }
             );
         });
+        menu = []; //Reset menu
     });
+}
+
+function exportAsOneFile(){
+    var allFiles = []; //Array to hold all tests as HTML
+
+    var files = fs.readdirSync(inputDir);
+    //Go through all .xml files and add HTML to the `allFiles` array
+    files.forEach(file => {
+        if(file.substr(-4) == '.xml'){
+            var data = fs.readFileSync(inputDir+'/'+file);//, 'utf8', function (err,data) {
+            xml2js.parseString(data,function(err,result){
+                //Add parsed XML -> HTML to `allFiles`
+                allFiles.push( parse(result) );
+            });
+        }
+    });
+
+    //Finish up the menu
+    cleanMenu();
+
+    fs.writeFile(
+        outputDir+'/alltests-'+generationDate+'.html',
+        HTMLVars.HTMLHeader+menu.join('')+allFiles.join("\n\n<!--New Result File-->\n")+HTMLVars.HTMLFooter, //Actually parse the input here
+        function(err){
+            if(err){
+                return console.log(err);
+            }
+        }
+    );
+    menu = []; //Reset menu
 
 }
 
 function generateFromFolder(){
+    //Check if `outputDir` exists
+    //if not, create it
+    var startTime = new Date().getUTCMilliseconds();
     if (!fs.existsSync(outputDir)){
         fs.mkdirSync(outputDir);
     }
 
     fs.readdir(inputDir, (err, files) => {
-        files.forEach(file => {
-            if(file.substr(-4) == '.xml'){
-                convertToHTML(file);
-            }
-        });
-    })
+        if(program.single){
+            //Will output all files as one
+            if(program.log)
+                console.log('Exporting files into one output.html');
+            exportAsOneFile();
+        } else {
+            //Will output all files individually
+            if(program.log)
+                console.log('Exporting files individually');
+            files.forEach(file => {
+                if(file.substr(-4) == '.xml'){
+                    convertSingleFileToHTML(file);
+                }
+            });
+        }
+        var endTime = new Date().getUTCMilliseconds();
+        var runTime = (endTime-startTime)/1000;
+        if(program.log){
+            console.log('HTML Report(s) Generated in `%s`\n\tDone in %sseconds',outputDir,runTime);
+        }
+    });
 }
